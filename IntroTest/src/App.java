f13282d13ecd712e3dca346fca20fd5077a76846
Argifari <argifari40@gmail.com>
2026-02-21 11:18:55 +0700

    Commit Contoh lengkap

--- IntroTest/src/App.java
+++ IntroTest/src/App.java
@@ -35,16 +35,61 @@ public class App {
         System.out.print("Kategori: ");
         if (umur < 17) {
             System.out.println("Remaja");
-            
         }else if (umur <= 25) {
             System.out.println("Dewasa Muda");
         }else {
             System.out.println("Dewasa");
-
         }
     }
 
     public static void contohArrayDanLooping () {
+        // Deklarasi dan Inisialisasi array
+        int[] nilaiUjian = {85, 92, 78, 95, 88};
+        String[] mataKuliah = {"Matematika", "Fisika", "Pemrograman", "Basis Data", "Algoritma"};
 
+        // Menampilkan data dengan for loop
+        System.out.println("Daftar Nilai:");
+        for (int i = 0; i < nilaiUjian.length; i++) {
+            System.out.println((i + 1) + ". " + mataKuliah[i] + ": " + nilaiUjian[i]);
+        }
+
+        // Menghitung total dan rata-rata menggunakan foreach loop
+        int total = 0;
+        for (int nilai : nilaiUjian) {
+            total += nilai;
+        }
+        double rataRata = (double) total / nilaiUjian.length;
+
+        System.out.println("\nTotal Nilai: " + total);
+        System.out.println("Rata-rata: " + rataRata);
+
+        // Mencari nilai tertinggi dan terendah dengan while loop
+        int tertinggi = nilaiUjian[0];
+        int terendah = nilaiUjian[0];
+        int index = 1;
+
+        while (index < nilaiUjian.length) {
+            if (nilaiUjian[index] > tertinggi) {
+                tertinggi = nilaiUjian[index];
+            }
+            if (nilaiUjian[index] < terendah) {
+                terendah = nilaiUjian[index];
+            }
+            index++;
+        }
+
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+        // Menghitung jumlah nilai yang lulus (>=75) dengando-while
+        int jumlahLulus = 0;
+        int idx = 0;
+        do {
+            if (nilaiUjian[idx] >= 75) {
+                jumlahLulus++;
+            }
+            idx++;
+        } while (idx < nilaiUjian.length);
+        
+        System.out.println("Jumlah Mata Kuliah yang Lulus: " + jumlahLulus + " dari " + nilaiUjian.length);
     }
 }
