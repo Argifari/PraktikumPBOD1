4418fac35a6e05fb3ddc242bd1628d95f02e2f02
Argifari <argifari40@gmail.com>
2026-02-26 08:53:44 +0000

    tugas_p2Cicil

--- praktikum2/src/Garis.java
+++ praktikum2/src/Garis.java
@@ -1,4 +1,34 @@
 public class Garis {
 
+    Titik titikAwal;
+    Titik titikAkhir;
+    static int counterGaris;
+
+    Garis() {
+        titikAwal = new Titik();
+        titikAkhir = new Titik(1,1);
+        counterGaris++;
+    }
+    Garis(Titik T1, Titik T2) {
+        titikAwal = T1;
+        titikAkhir = T2;
+        counterGaris++;
+    }
+
+    Titik getTitikAwal() {
+        return titikAwal;
+    }
+    Titik getTitikAkhir() {
+        return titikAkhir;
+    }
+    void setTitikAwal(Titik T) {
+        titikAwal = T;
+    }
+    void setTitikAkhir(Titik T) {
+        titikAkhir = T;
+    }
     
+    static int getCounterGaris() {
+        return counterGaris;
+    }
 }
