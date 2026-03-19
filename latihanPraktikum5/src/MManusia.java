1f8e9f75f90d2c1d96a536e631fa3a109bd4ab2f
Argifari <argifari40@gmail.com>
2026-03-19 08:38:11 +0700

    Selesai-latihan-pertemuan5

--- latihanPraktikum5/src/MManusia.java
+++ latihanPraktikum5/src/MManusia.java
@@ -1,3 +1,11 @@
+/*
+Nama file : Manusia.java
+Nama : Muhammad Firdaus Argifari
+NIM : 24060124130107
+Tanggal : 18 Maret 2026
+Deskripsi : main driver clas MManusia
+*/
+
 import java.time.LocalDate;
 
 public class MManusia {
